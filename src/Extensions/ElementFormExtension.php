b14cce16d7809f564759065b6604510a85a47b07
Rastislav Brandobur <brandobur@tjbw.sk>
2023-10-03 14:18:19 +0200

    added support for counter display in userform field groups

--- src/Extensions/ElementFormExtension.php
+++ src/Extensions/ElementFormExtension.php
@@ -9,10 +9,12 @@ use SilverStripe\ORM\DataExtension;
 class ElementFormExtension extends DataExtension
 {
     private static $db = [
+        'ShowCounterOnFieldGroups' => 'Boolean',
         'StartButton' => 'Boolean',
     ];
 
     private static $field_labels = [
+        'ShowCounterOnFieldGroups' => 'Zobraziť očíslovanie obaľovacích skupín',
         'StartButton' => 'Spúšťacie tlačidlo odoslania (so šípkou)',
     ];
 
@@ -23,6 +25,12 @@ class ElementFormExtension extends DataExtension
 
     public function updateFormOptions(&$options)
     {
+        $options->insertBefore('ShowClearButton', CheckboxField::create(
+            'ShowCounterOnFieldGroups',
+            $this->owner->fieldLabel('ShowCounterOnFieldGroups'),
+            $this->owner->ShowCounterOnFieldGroups,
+        ));
+
         $options->insertBefore('ShowClearButton', CheckboxField::create(
             'StartButton',
             $this->owner->fieldLabel('StartButton'),
