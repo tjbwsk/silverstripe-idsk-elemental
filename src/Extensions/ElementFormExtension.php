2d654dd6d9c1e0879a97c8fadc3239592fcaa943
Rastislav Brandobur <brandobur@tjbw.sk>
2023-10-03 14:17:26 +0200

    added "start button" style support for userform process action

--- src/Extensions/ElementFormExtension.php
+++ src/Extensions/ElementFormExtension.php
@@ -2,13 +2,31 @@
 
 namespace TJBW\IdSkElemental\Extensions;
 
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\ORM\DataExtension;
 
 class ElementFormExtension extends DataExtension
 {
+    private static $db = [
+        'StartButton' => 'Boolean',
+    ];
+
+    private static $field_labels = [
+        'StartButton' => 'Spúšťacie tlačidlo odoslania (so šípkou)',
+    ];
+
     public function updateCMSFields(FieldList $fields)
     {
         $fields->removeByName('EnableLiveValidation');
+    }
+
+    public function updateFormOptions(&$options)
+    {
+        $options->insertBefore('ShowClearButton', CheckboxField::create(
+            'StartButton',
+            $this->owner->fieldLabel('StartButton'),
+            $this->owner->StartButton,
+        ));
     }
 }
